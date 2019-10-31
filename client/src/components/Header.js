e7bdc29e370a6003f0b113ac7d3229859e76ecc8
cam32099 <camperk320@outlook.com>
2019-10-31 11:29:34 -0400

    Front-End

---
+++ client/src/components/Header.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import HelpIcon from '@material-ui/icons/Help';
+import Hidden from '@material-ui/core/Hidden';
+import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import PersonIcon from '@material-ui/icons/Person';
+import Popover from '@material-ui/core/Popover';
+import Tab from '@material-ui/core/Tab';
+import Tabs from '@material-ui/core/Tabs';
+import Toolbar from '@material-ui/core/Toolbar';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const lightColor = 'rgba(255, 255, 255, 0.7)';
+
+const styles = theme => ({
+  secondaryBar: {
+    zIndex: 0,
+  },
+  menuButton: {
+    marginLeft: -theme.spacing(1),
+  },
+  iconButtonAvatar: {
+    padding: 4,
+  },
+  link: {
+    textDecoration: 'none',
+    color: lightColor,
+    '&:hover': {
+      color: theme.palette.common.white,
+    },
+  },
+  button: {
+    borderColor: lightColor,
+  },
+});
+
+function Header(props) {
+  const { classes, onDrawerToggle } = props;
+  const [isLogedIn, setIsLogedIn] = React.useState(false)
+  const [hasAccount, setHasAccount] = React.useState(true)
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const handleClick = event => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+  return (
+    <React.Fragment>
+      <AppBar color="primary" position="sticky" elevation={0}>
+        <Toolbar>
+          <Grid container spacing={1} alignItems="center">
+            <Hidden smUp>
+              <Grid item>
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={onDrawerToggle}
+                  className={classes.menuButton}
+                >
+                  <MenuIcon />
+                </IconButton>
+              </Grid>
+            </Hidden>
+            <Grid item xs />
+            <Grid item>
+              <Tooltip title="Alerts • No alerts">
+                <IconButton color="inherit">
+                  <NotificationsIcon />
+                </IconButton>
+              </Tooltip>
+            </Grid>
+            <Grid item>
+              <IconButton color="inherit" onClick={handleClick}>
+                <PersonIcon />
+              </IconButton>
+              <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+              >
+                <List component="log">
+                      <ListItem button>
+                        <ListItemText primary="Log In" />
+                      </ListItem>
+                      <ListItem button>
+                        <ListItemText primary="Sign Up" />
+                      </ListItem>
+                </List>
+              </Popover>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <AppBar
+        component="div"
+        className={classes.secondaryBar}
+        color="primary"
+        position="static"
+        elevation={0}
+      >
+        <Toolbar>
+          <Grid container alignItems="center" spacing={1}>
+            <Grid item xs>
+              <Typography color="inherit" variant="h4" component="h1">
+                Search
+              </Typography>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+      <AppBar
+        component="div"
+        className={classes.secondaryBar}
+        color="primary"
+        position="static"
+        elevation={0}
+      >
+       
+      </AppBar>
+    </React.Fragment>
+  );
+}
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+  onDrawerToggle: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(Header);
---
+++ client/src/components/Navigator.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { withStyles } from '@material-ui/core/styles';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import InfoIcon from '@material-ui/icons/Info';
+import NewReleasesIcon from '@material-ui/icons/NewReleases';
+import PaymentIcon from '@material-ui/icons/Payment';
+import PeopleIcon from '@material-ui/icons/People';
+import DnsRoundedIcon from '@material-ui/icons/DnsRounded';
+import PermMediaOutlinedIcon from '@material-ui/icons/PhotoSizeSelectActual';
+import PublicIcon from '@material-ui/icons/Public';
+import SettingsEthernetIcon from '@material-ui/icons/SettingsEthernet';
+import SettingsInputComponentIcon from '@material-ui/icons/SettingsInputComponent';
+import TimerIcon from '@material-ui/icons/Timer';
+import SearchIcon from '@material-ui/icons/Search';
+import SettingsIcon from '@material-ui/icons/Settings';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import PhonelinkSetupIcon from '@material-ui/icons/PhonelinkSetup';
+import logo from './imgs/FixThisLogoSimple.png'; 
+
+const categories = [
+  {
+    id: 'Reports',
+    children: [
+      { id: 'Featured', icon: <StarBorderIcon />, active: true },
+      { id: 'Following', icon: <PeopleIcon /> },
+      { id: 'New', icon: <NewReleasesIcon /> },
+      { id: 'Search', icon: <SearchIcon /> },
+    ],
+  },
+  {
+    id: 'Account',
+    children: [
+      { id: 'Subscriptions', icon: <PaymentIcon /> },
+      { id: 'Status', icon: <InfoIcon /> },
+      { id: 'Settings', icon: <SettingsIcon /> },
+    ],
+  },
+];
+
+const styles = theme => ({
+  categoryHeader: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+  categoryHeaderPrimary: {
+    color: theme.palette.common.white,
+  },
+  item: {
+    paddingTop: 1,
+    paddingBottom: 1,
+    color: 'rgba(255, 255, 255, 0.7)',
+    '&:hover,&:focus': {
+      backgroundColor: '#f09713', //orange
+    },
+  },
+  itemCategory: {
+    backgroundColor: '#262525', //black
+    boxShadow: '0 -1px 0 #f09713 inset',
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+  firebase: {
+    fontSize: 24,
+    color: theme.palette.common.white,
+  },
+  itemPrimary: {
+    fontSize: 'inherit',
+  },
+  itemIcon: {
+    minWidth: 'auto',
+    marginRight: theme.spacing(2),
+  },
+  divider: {
+    marginTop: theme.spacing(2),
+  },
+});
+
+function Navigator(props) {
+  const { classes, ...other } = props;
+
+  return (
+    <Drawer variant="permanent" {...other}>
+      <List disablePadding>
+        <ListItem className={clsx(classes.firebase, classes.itemCategory)}>
+          <img className={classes.img} alt="FixThis" src={logo} width="200" height="65"/>
+        </ListItem>
+        {categories.map(({ id, children }) => (
+          <React.Fragment key={id}>
+            <ListItem className={classes.categoryHeader}>
+              <ListItemText
+                classes={{
+                  primary: classes.categoryHeaderPrimary,
+                }}
+              >
+                {id}
+              </ListItemText>
+            </ListItem>
+            {children.map(({ id: childId, icon, active }) => (
+              <ListItem
+                key={childId}
+                button
+                className={clsx(classes.item, active && classes.itemActiveItem)}
+              >
+                <ListItemIcon className={classes.itemIcon}>{icon}</ListItemIcon>
+                <ListItemText
+                  classes={{
+                    primary: classes.itemPrimary,
+                  }}
+                >
+                  {childId}
+                </ListItemText>
+              </ListItem>
+            ))}
+
+            <Divider className={classes.divider} />
+          </React.Fragment>
+        ))}
+      </List>
+    </Drawer>
+  );
+}
+
+Navigator.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Navigator);
