33ba7eff7538dbaa5e81b1802043d97a249fda15
colinchu1 <54604227+colinchu1@users.noreply.github.com>
2019-12-09 13:17:38 -0500

    sdfdsfs

--- project1/Components/AppTabNavigator/Signin.js
+++ project1/Components/AppTabNavigator/Signin.js
@@ -9,17 +9,77 @@ import {
 } from "react-native";
 import Animated, { Easing } from 'react-native-reanimated';
 import {Actions } from 'react-native-router-flux';
+import * as axios from 'axios';
 
 const { width, height } = Dimensions.get('window');
 
 export default class Signin extends React.Component {
-    
+  state = {
+    username: '',
+    password: '',
+    auth_token: ''
+  }
+  handleSubmit = event => {
+    event.preventDefault()
+    axios
+      .post('/user/login', {
+        username: this.state.username,
+        password: this.state.password
+      })
+      .then(response => {
+        JWT.keep(response.data.token)
+        setPage('home')
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+ /* constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('http://192.168.1.70/user/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      }),
+    })
+    .then((response) => 
+    {
+      if (response.status == 200)
+      {
+        console.log("it work") 
+      } else {
+        console.log("it not work")
+      }
+      response.json()
+    })
+    .then((responseJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: "login maybe",
+        }, function(){
+        });
+
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+  }*/
+
   render() {
       return(
         <View style = {styles.container}>
-        <TextInput placeholder = "Username"  style = {styles.textInput} placeholderTextColor = "black" />
-            <TextInput placeholder = "password"  style = {styles.textInput} placeholderTextColor = "black" />
-            <TouchableOpacity onPress={() => this.props.navigation.navigate("Home")}>
+        <TextInput placeholder = "Username" onChangeText={ TextInputValue => this.setState({username : TextInputValue }) } style = {styles.textInput} placeholderTextColor = "black" />
+            <TextInput placeholder = "password" onChangeText={ TextInputValue => this.setState({password : TextInputValue }) } style = {styles.textInput} placeholderTextColor = "black" />
+            <TouchableOpacity onPress={this.handleSubmit}>
             <Animated.View style = {styles.button1}>
               <Text style = {{fontSize: 20, fontWeight: 'bold',color: 'white'}}>
                 SIGN IN
